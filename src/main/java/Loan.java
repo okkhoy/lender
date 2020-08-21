2d7de402d77fa824d1e2e1f5761a8d8274dc8001
Akshay <okkhoy@gmail,com>
2020-08-21 17:20:21 +0800

    Add maxLoanAmount variable to track the max value lent so far

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -4,13 +4,23 @@ public class Loan {
     private boolean isPaid;
     private double interestRate;
 
+    private static int maxLoanAmount = 0;
+
     public Loan(String borrower, int amount, double interestRate) {
         this.borrower = borrower;
         this.amount = amount;
         this.interestRate = interestRate;
         this.isPaid = false;
+
+        if (amount > maxLoanAmount) {
+            maxLoanAmount = amount;
+        }
     }
 
+    public static int getMaxLoanAmount() {
+        return maxLoanAmount;
+    }
+    
     public void setAsPaid() {
         isPaid = true;
     }
